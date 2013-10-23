856b8dbe1abd72bb7caff2d6dac2381d6df7e89a
Douwe van der Meij <vandermeij@gw20e.com>
2013-10-23 16:07:43 +0200

    added urls to serve uploaded themes

--- django_diazo_blog/urls.py
+++ django_diazo_blog/urls.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 import django_diazo_themes.ministerial.view
 
@@ -16,3 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+       url(r'^%s/themes/(?P<path>.*)$' % settings.MEDIA_URL.strip('/'), 'django.views.static.serve',
+           {'document_root': os.path.join(settings.MEDIA_ROOT, 'themes'), 'show_indexes': True}),
+    )
